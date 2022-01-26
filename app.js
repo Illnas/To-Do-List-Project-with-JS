68a9b74d390e2931b61b63d730a868498d3321d5
Nadatari <programer10@ucione.local>
2022-01-26 18:44:50 +0100

    Functionality fully working

--- app.js
+++ app.js
@@ -1,52 +1,38 @@
 let sectionContainer = document.getElementById("section-container");
-let submit = document.getElementById("submit");
-let clear = document.getElementById("clear")
 let itemContainer = document.getElementsByClassName("item-container");
-let paragraph = document.getElementsByClassName("paras");
 let buttons = document.getElementsByClassName("buttons");
 let inputVal = document.getElementById("inputVal");
+let main = document.getElementById("main")
 
 
 
-submit.onclick = (e) => {
-  e.preventDefault();
-  let emptyArr = [];
-  console.log(inputVal.value);
-  emptyArr.push(inputVal.value);
-  console.log(emptyArr);
-  inputVal.value = ""
+function addingEelements(arr) {
+  arr.map((e) => {
+    const itemContainer = document.createElement("div");
+    itemContainer.classList.add("item-container");
+    sectionContainer.appendChild(itemContainer);
 
-  emptyArr.map(e => {
-  const itemContainer = document.createElement("div");
-  itemContainer.classList.add("item-container");
-  sectionContainer.appendChild(itemContainer);
+    const paragraphItem = document.createElement("p");
+    paragraphItem.innerText = `${e}`;
+    itemContainer.appendChild(paragraphItem);
 
-  const paragraphItem = document.createElement("p");
-  paragraphItem.innerText = `${e}`;
-  itemContainer.appendChild(paragraphItem);
+    const buttonContainer = document.createElement("div");
+    buttonContainer.classList.add("buttons");
+    itemContainer.appendChild(buttonContainer);
 
-  const buttonContainer = document.createElement("div");
-  buttonContainer.classList.add("buttons");
-  itemContainer.appendChild(buttonContainer);
+    const buttonFinished = document.createElement("button");
+    const buttonRemoved = document.createElement("button");
+    buttonFinished.innerHTML = `<span class="material-icons" data-action="finished"> check </span>`;
+    buttonRemoved.innerHTML = `<span class="material-icons" data-action="remove"> close </span>`;
 
-  const buttonFinished = document.createElement("button");
-  const buttonRemoved = document.createElement("button");
-  buttonFinished.innerHTML = `<span class="material-icons" data-action="finished"> check </span>`
-  buttonRemoved.innerHTML = `<span class="material-icons" data-action="remove"> close </span>`
-
-
-  buttonContainer.appendChild(buttonFinished);
-  buttonContainer.appendChild(buttonRemoved);
-  })
-};
-
-clear.onclick = () => {
-    sectionContainer.innerHTML = ""
+    buttonContainer.appendChild(buttonFinished);
+    buttonContainer.appendChild(buttonRemoved);
+  });
 }
 
 
 
-sectionContainer.onclick = (e) => {
+main.onclick = (e) => {
   let actions = e.target.dataset.action;
   let sibling = e.target.parentNode.parentNode.previousElementSibling;
   if (actions === "remove") {
@@ -55,5 +41,20 @@ sectionContainer.onclick = (e) => {
 
   if (actions === "finished") {
     sibling.classList.toggle("paras");
+  }
+
+  if(actions === "clear") {
+    sectionContainer.innerHTML = "";
+  }
+
+  if(actions === "submit") {
+    e.preventDefault();
+    let emptyArr = [];
+    emptyArr.push(inputVal.value);
+    if (emptyArr[0] !== "") {
+      addingEelements(emptyArr);
+    }
+    inputVal.value = "";
+    console.log(emptyArr);
   }
 };
